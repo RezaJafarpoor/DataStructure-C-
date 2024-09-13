f6ea9284fb7e018dc7b301d17c0b8bbee35366e0
reza <rezajafarpoor222@gmail.com>
2024-09-13 20:48:41 +0330

    stack added

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -1,19 +1,4 @@
 ﻿
-using DataStructure;
-using DataStructure.Sorting;
-
-
-
-
- 
-
- var ordered = MergeSort.Sort(Data.StaticData);
- foreach (var VARIABLE in ordered)
- {
-     Console.Write(VARIABLE + " ");
- }
-Console.WriteLine();
-Console.WriteLine("number of Divides: "+ MergeSort.Divides);
 
 
 
---
+++ DataStructure/Stack/Stack.cs
@@ -0,0 +1,39 @@
+﻿namespace DataStructure.Stack;
+
+public class Stack
+{
+    public int[] stackArray = new int[10];
+    private int index = -1;
+
+    public void Push(int value)
+    {
+        if (index <=9)
+        {
+            index++;
+            stackArray[index] = value;
+        }
+        else
+        {
+            Console.WriteLine("Stack is full");
+        }
+        
+    }
+
+
+    public void Pop()
+    {
+        if (index == -1)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine(stackArray[index]);
+            stackArray[index] = default;
+            index--;
+        }
+        
+        
+    }
+    
+}
